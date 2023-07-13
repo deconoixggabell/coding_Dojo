135902675b1e4459ae6e194b352fc2b713b96500
deconoixggabell <deconoixggabell@gmail.com>
2023-07-12 19:00:52 -0600

    js

--- Coding_Dojo/web fund/notes/html_Css_JS/Main.js
+++ Coding_Dojo/web fund/notes/html_Css_JS/Main.js
@@ -1,1 +1,22 @@
-// JS recap
+console.log(document)
+// option 1
+// let number = document.getElementsByClassName("counting_num")[0]
+
+// option 2
+let number = document.querySelector(".counting_num")
+
+function alartme(){
+    alert("hello World")
+}
+
+function increasenum(num){
+    console.log(num)
+    console.log(num.innerText)
+    // console.log(typeof(num.innerText))
+    num.innerText = parseInt(num.innerText) + 1
+}
+
+console.log()
+function increasenum2(){
+    number.innerText = parseInt(number.innerText) + 1
+}
