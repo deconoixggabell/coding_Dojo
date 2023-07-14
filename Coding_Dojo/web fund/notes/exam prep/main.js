dd6505649d9d443f7041392c1b7ea568da531fef
deconoixggabell <deconoixggabell@gmail.com>
2023-07-13 19:24:11 -0600

    Mock Exam

---
+++ Coding_Dojo/web fund/notes/exam prep/main.js
@@ -0,0 +1,30 @@
+console.log('running main.js')
+
+// disocount pop up
+// option one
+// function discountCode(){
+//     alart("save 20% with code Hero ")
+// }
+// setTimeout (discountCode, 10000)
+
+
+// setTimeout(function(){
+//     alert("save code 20% with code Hero20")
+// }, 10000)
+
+
+// // when user clicks an alart with searchbar
+// let form = document.querySelector('form')
+// let searchInput = document.querySelector('input')
+// // console.log(searchInput.value)
+
+// // function searchalart(){
+// //     alert(searchInput.value)
+// // }
+
+// form.addEventListener('submit', function(){
+//     alert(searchInput.value)
+// })
+
+
+// increase the card amount
