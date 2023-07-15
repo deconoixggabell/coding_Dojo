70fbf65f187683edb5c1dfefdae2646174034bdf
deconoixggabell <deconoixggabell@gmail.com>
2023-07-15 10:47:50 -0600

    Button clock

---
+++ Coding_Dojo/web fund/Asigment/Button Clicker/main.js
@@ -0,0 +1,18 @@
+
+// Log out - Log out button
+function log_out(){
+    document.querySelector('#log_out')
+    .textContent = 'logout'
+}
+
+// Buton Function
+function button_1(){
+    document.querySelector('#button_1,#button_2')
+    alert("ninja was liked")
+}
+
+// Definition button
+function definition_button(){
+    document.querySelector('#definition_button')
+    .style.display = 'none'
+}
