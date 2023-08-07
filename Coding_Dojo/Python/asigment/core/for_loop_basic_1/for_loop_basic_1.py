f7362e83629cf454af285871d5c92df5b8827189
deconoixggabell <deconoixggabell@gmail.com>
2023-08-07 09:45:20 -0500

    python

---
+++ Coding_Dojo/Python/asigment/core/for_loop_basic_1/for_loop_basic_1.py
@@ -0,0 +1,17 @@
+
+# for i in range(0,150):
+#     print(i)
+
+# for i in range(5,1000,5):
+#     print(i)
+
+# for i in range(1,100):
+#     print(i)
+#     if i in range(5,100,5):
+#         print("Coding")
+#     if i in range(10,100,10):
+#         print("Coding Dojo")
+
+
+# for i in range(2018,0,-4):
+#     print(i)
