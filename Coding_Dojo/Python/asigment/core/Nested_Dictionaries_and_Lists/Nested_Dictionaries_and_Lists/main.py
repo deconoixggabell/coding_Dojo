832f1d226beba04f6b633020357763db9deb495b
deconoixggabell <deconoixggabell@gmail.com>
2023-08-13 10:41:05 -0500

    Nested dictionaries and lists

--- Coding_Dojo/Python/asigment/core/Nested_Dictionaries_and_Lists/Nested_Dictionaries_and_Lists/main.py
+++ Coding_Dojo/Python/asigment/core/Nested_Dictionaries_and_Lists/Nested_Dictionaries_and_Lists/main.py
@@ -24,40 +24,43 @@ sports_directory['soccer'][0] = "andres"
 print(sports_directory)
 
 # Part 2
-def iterateDictionary(students):
-    while students:
-        print(students)
-        break
+students = [
+        {'first_name':  'Michael', 'last_name' : 'Jordan'},
+        {'first_name' : 'John', 'last_name' : 'Rosales'},
+        {'first_name' : 'Mark', 'last_name' : 'Guillen'},
+        {'first_name' : 'KB', 'last_name' : 'Tonel'}
+    ]
+def iterateDictionary(list):
+    for i in range(0, len(list)-1):
+        output = ""
+        for key,val in list[i].items():
+            output += f"{key} - {val},"
+        print(output)
+
+            # print(f" {'first_name'} - {'last_name'}")
 iterateDictionary(students)
 
-# part 3 - 1
-def iterateDictionary2(students):
-    while students:
-        first_name1 = students[0]['first_name']
-        first_name2 = students[1]['first_name']
-        print(first_name1, first_name2)
-        break
-iterateDictionary2(students)
+# part 3
 
-# part 3 - 2
-def iterateDictionary3(students):
-    while students:
-        last_name1 = students[0]['last_name']
-        last_name2 = students[1]['last_name']
-        print(last_name1, last_name2)
-        break
-iterateDictionary3(students)
+def iterateDictionary2(key_name,list):
+    for i in range(0, len(list)):
+        for key,val in list[i].items():
+            if key == key_name:
+                print(val)
+            
+iterateDictionary2('first_name',students)
+print(' ')
+iterateDictionary2('last_name', students)
 
 # part 4
-def printinfo(dict):
-    for key,val in dict.items():
-        print(f"{len(val)} {key.upper}")
-        for i in range(0,len.val):
-            print(val[i])
-
-
 dojo = {
 'locations': ['San Jose', 'Seattle', 'Dallas', 'Chicago', 'Tulsa', 'DC', 'Burbank'],
 'instructors': ['Michael', 'Amy', 'Eduardo', 'Josh', 'Graham', 'Patrick', 'Minh', 'Devon']
 }
-print(dojo)
+
+def printinfo(dict):
+    for key,val in dict.items():
+        print(f"{len(val)} {key.upper()}")
+        for i in range(0,len(val)):
+            print(val[i])
+printinfo(dojo)
