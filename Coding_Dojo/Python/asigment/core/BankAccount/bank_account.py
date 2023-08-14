33f725f49794dc47ea3b154a8785a0669230f9cb
deconoixggabell <deconoixggabell@gmail.com>
2023-08-14 15:49:37 -0400

    Bank acount

--- Coding_Dojo/Python/asigment/core/BankAccount/bank_account.py
+++ Coding_Dojo/Python/asigment/core/BankAccount/bank_account.py
@@ -1,15 +1,15 @@
 class BankAccount:
     # don't forget to add some default values for these parameters!
-    def __init__(self, int_rate, balance = 200) -> None: 
+    def __init__(self, int_rate) -> None: 
         self.int_rate = int_rate
-        self.balance = balance
+        self.balance = 100
         # your code here! (remember, instance attributes go here)
         # don't worry about user info here; we'll involve the User class soon
     def deposit(self, amount):
         new_balence = self.balance + amount
         return new_balence
         # your code here
-    deposit(new_balance, amount = 20)
+    deposit(self, amount = 20)
     # def withdraw(self, amount):
     #     # your code here
     # def display_account_info(self):
---
+++ Coding_Dojo/Python/asigment/practice/user/user.py
@@ -0,0 +1,17 @@
+class user:
+    def __init__(self, first_name, last_name, email, age) -> None:
+        self.first_name = first_name
+        self.last_name = last_name
+        self.email = email
+        self.age = age
+
+    def display_info(self):
+        for i in len(self):
+            pass
+
+
+    def enroll(self):
+        pass
+
+    def spend_points(self,amount):
+        pass
