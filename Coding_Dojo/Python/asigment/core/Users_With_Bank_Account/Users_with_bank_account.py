cc7e00e2448ce8df33a821aff9499100ebdb600f
deconoixggabell <deconoixggabell@gmail.com>
2023-08-19 21:12:22 -0600

    user with account final

--- Coding_Dojo/Python/asigment/core/Users_With_Bank_Account/Users_with_bank_account.py
+++ Coding_Dojo/Python/asigment/core/Users_With_Bank_Account/Users_with_bank_account.py
@@ -1,7 +1,6 @@
 
 class BankAccount:
     def __init__(self,int_rate, balance) -> None: 
-        pass
         self.int_rate = int_rate
         self.balance = balance
 
@@ -10,30 +9,30 @@ class User:
         self.name = name
         self.email = email
         self.account = BankAccount(int_rate=0.02, balance=0)
-        
-
 
     def make_deposit(self, amount):
-        self.account = int(amount)
-        self.account += amount
-        return
-        # print(f"Your Balance is ${amount}")
+        new_var = self.account = amount
+        new_var += amount
 
     def withdraw(self, amount):
         if amount < self.account:
             self.account -= amount + 5
-            # print(f" Your new Balance: ${self.account}")
         else:
             print("Insufficient funds")
 
 user1 = User("Mark","mark@email.com")
 user1.make_deposit(100)
 print(f"User:{user1.name} Balance ${user1.account}")
-# user1.withdraw(10)
-
-print("--------------------")
+user1.withdraw(10)
+print(f"User:{user1.name} New Balance ${user1.account}")
 
 user2 = User("Jim", "jim@email.com")
 user2.make_deposit(1000)
 print(f"User:{user2.name} Balance ${user2.account}")
-# user2.withdraw(200)
+user2.withdraw(200)
+print(f"User:{user2.name} New Balance ${user2.account}")
+
+
+''''SENSEI BONUS: Allow a user to have multiple accounts; update methods so the user has to specify which account they are withdrawing or depositing to
+
+SENPAI BONUS: Add a transfer_money(self, amount, other_user) method to the user class that takes an amount and a different User instance, and transfers money from the user's account into another user's account.'''
