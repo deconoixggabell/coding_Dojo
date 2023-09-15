aa1dfe61ce7bb3cc98b0cfda3223f3877ddd18fc
deconoixggabell <deconoixggabell@gmail.com>
2023-09-15 16:46:01 -0600

    user crud mod final

--- Coding_Dojo/Python/asigment/core/Playground/server.py
+++ Coding_Dojo/Python/asigment/core/Playground/server.py
@@ -1,19 +1,19 @@
-from flask import Flask, render_template
+from flask import Flask, render_template, redirect
 app = Flask(__name__)
 
 @app.route('/')
 def index():
-    return render_template("index.html", phase="hello", times=5)
+    return redirect('/play')
 
 @app.route('/play')
 def play_1():
     return render_template("index.html", num=3, color="blue")
 
-@app.route('/play/num')
+@app.route('/play/<int:num>')
 def play_2(num):
     return render_template("index.html",num = num, color="blue")
 
-@app.route('/play/num/color')
+@app.route('/play/<int:num>/<color>')
 def play_3(num,color):
     return render_template("index.html", num = num, color = color)
 
