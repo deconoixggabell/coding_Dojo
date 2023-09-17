6193b8e2355e54f6e13fbda8cad9d24f88287f45
deconoixggabell <deconoixggabell@gmail.com>
2023-09-16 20:20:23 -0600

    login and registration

---
+++ Coding_Dojo/Python/asigment/core/login_and_registration/flask_app/controllers/controller.py
@@ -0,0 +1,98 @@
+from flask import render_template,request, redirect, session, flash, Request
+from flask_app import app 
+from flask_bcrypt import bcrypt
+from flask_app.models.user import User
+bcrypt = bcrypt(app)
+
+@app.route('/')
+def index():
+    return redirect('/index')
+
+@app.route('/index')
+def reg_log():
+    return render_template("index.html")
+
+@app.route('/index/register',method=['POST'])
+def register():
+    if not User.is_valid(request.form):
+        return redirect('/')
+    data = {
+        "first_name": request.form['first_name'],
+        "last_name": request.form['last_name'],
+        "email": request.form['email'],
+        "password": bcrypt.generate_password_hash(request.form['password'])
+    }
+    id = User.add(data)
+    session['user_id'] = id
+
+    return redirect('/home')
+
+@app.route('/login',method=['POST'])
+def login():
+    user = User.get_email(request.form)
+    if not user:
+        flash("Invalid Email")
+        return redirect('/')
+    if not bcrypt.check_password_hash(user.password,request.form['password']):
+        flash("wrong password")
+        return redirect('/')
+    session['user_id'] = user.id
+    return redirect('/home')
+
+@app.routr('/home')
+def home():
+    if 'user_id' not in session:
+        return redirect('/logout')
+    data = {
+        "id":['user_id']
+    }
+    return render_template("home.html", User = User.get_id(data))
+
+@app.route('/logout')
+def logout():
+    session.clear()
+    return redirect('/')
+
+# @app.route('/users/create/',methods=['POST'])
+# def register(password):
+#     data={
+#         "password":password
+#     }
+#     confirm_password=password
+#     if password == confirm_password:
+#         User.add(request.form)
+#         return redirect('/users')
+#         flash("accout created")
+#     else:
+#         flash('Your password does not match')
+
+# @app.route('/users/new_user/')
+# def new_user():
+#     return render_template("new_user.html")
+
+# @app.route('/users/edit/<int:id>')
+# def edit_user(id):
+#     data ={
+#         "id":id
+#     }
+#     return render_template("edit_user.html",user=User.get_one(data))
+
+# @app.route('/users/update/',methods=['POST'])
+# def update():
+#     User.update(request.form)
+#     return redirect('/users')
+
+# @app.route('/users/show/<int:id>')
+# def show(id):
+#     data ={
+#         "id":id
+#     }
+#     return render_template("show_user.html",user=User.get_one(data))
+
+# @app.route('/users/delete/<int:id>')
+# def delete(id):
+#     data = {
+#         "id":id
+#     }
+#     User.delete(data)
+#     return redirect('/users')
---
+++ Coding_Dojo/Python/asigment/core/login_and_registration/flask_app/models/user.py
@@ -0,0 +1,89 @@
+from flask_app.config.mysqlconnection import connectToMySQL
+import re
+from flask import flash
+EMAIL_REGEX = re.compile(r'^[a-zA-Z0-9.+_-]+@[a-zA-Z0-9._-]+\.[a-zA-Z]+$')
+from flask import flask
+
+class User:
+    def __init__(self, data) -> None:
+        self.id=data['id']
+        self.first_name = data['first_name']
+        self.last_name = data['last_name']
+        self.email = data['email']
+        self.password = data['password']
+        self.created_at = data['created_at']
+
+    @classmethod
+    def add(cls, data):
+        query = "INSERT INTO reg_login.users (first_name, last_name, email,password,created_at) VALUES(%(first_name)s,%(last_name)s,%(email)s,%(password)s,NOW());"
+        result = connectToMySQL('reg_login').query_db(query,data)
+        return result
+    
+    # def full_name(self):
+    #     return f"{self.first_name}{self.last_name}"
+
+    @classmethod
+    def get_all(cls):
+        query = "SELECT * FROM users;"
+        results = connectToMySQL('reg_login').query_db(query)
+        users = []
+        for u in results:
+            users.append(cls(u))
+        return users
+    
+    @classmethod
+    def get_id(cls,data):
+        query = "SELECT * FROM reg_login.users WHERE id = %(id)s;"
+        result = connectToMySQL('reg_login').query_db(query,data)
+        return cls(result[0])
+
+    @classmethod
+    def get_email(cls,data):
+        query = "SELECT * FROM reg_login.users WHERE email = %(email)s;"
+        result = connectToMySQL('reg_login').query_db(query,data)
+        if len(result) < 1:
+            return False
+        return cls(result[0])
+    
+    @staticmethod
+    def validate_reg(user):
+        is_valid = True
+        query = "SELECT * FROM reg_login.users WHERE email = %(email)s;"
+        result = connectToMySQL(User.reg_login).query_db(query,user)
+        if len(result) > 1:
+            flash("Email taken.")
+            is_calide = False
+        if not EMAIL_REGEX.match(user['email']):
+            flash("email cann't be used")
+            is_valid = False
+        if len(user['first_name'] < 1):
+            flash("First name must be more than one charactor")
+            is_valid = False
+        if len(user['last_name'] < 1):
+            flash("Last name must be more than 1 charactor")
+            is_valid = False
+        if len(user['Password'] < 8):
+            flash("password must be at leat 8 charactor")
+            is_valid = False
+        if user['password'] != user['confirm_password']:
+            flash("Password do not match")
+        return is_valid
+    
+    # @staticmethod
+    # def val_login(user):
+    #     is_valid = True
+    #     query = "SELECT * FROM reg_login.users WHERE email = %(email)s;"
+    #     result = connectToMySQL(User.reg_login).query_db(query,user)
+    #     return is_valid
+
+
+
+    # @classmethod
+    # def update(cls,data):
+    #     query = "UPDATE users SET first_name=%(first_name)s,last_name=%(last_name)s,email=%(email)s, password=NOW() WHERE id=%(id)s;"
+    #     return connectToMySQL('users').query_db(query,data)
+    
+    # @classmethod
+    # def delete(cls,data):
+    #     query = "DELETE FROM users WHERE id = %(id)s;"
+    #     return connectToMySQL('users').query_db(query,data)
